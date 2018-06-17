a6cd3ce1d3b49545f1655b755e6012a391c59f11
Madistia <qdf16@mails.tsinghua.edu.cn>
2018-06-17 21:30:43 +0800

    fix bugs in jumper.py/main.py

--- src/jump_python/jumper.py
+++ src/jump_python/jumper.py
@@ -7,35 +7,34 @@ import ai
 ai.init()
 
 def read_image():
-	# if your OS is win
-    os.system("../../dependency/platform-tools-windows/adb.exe shell screencap -p autojump.png")
-    os.system("../../dependency/platform-tools-windows/adb.exe pull /sdcard/autojump.png .")
-	## if your OS is mac 
-	# os.system("../../dependency/platform-tools-macos/adb shell screencap -p autojump.png")
-	# os.system("../../dependency/platform-tools-macos/adb pull /sdcard/autojump.png .")
+    # if your OS is win
+    #os.system("../../dependency/platform-tools-windows/adb.exe shell screencap -p /sdcard/autojump.png")
+    #os.system("../../dependency/platform-tools-windows/adb.exe pull /sdcard/autojump.png .")
+    ## if your OS is mac 
+    os.system("../../dependency/platform-tools-macos/adb shell screencap -p /sdcard/autojump.png")
+    os.system("../../dependency/platform-tools-macos/adb pull /sdcard/autojump.png .")
     im = Image.open("autojump.png")
     return im
 
 
 def click_screen(press_time):
-	# if your OS is win 
-    os.system("../../dependency/platform-tools-windows/adb.exe swipe 50 50 50 50 " + str(int(press_time)))
-	## is your OS is mac 
-	# os.system("../../dependency/platform-tools-macos/adb swipe 50 50 50 50 " + str(int(press_time)))
+    # if your OS is win 
+    #os.system("../../dependency/platform-tools-windows/adb.exe shell input swipe 50 50 50 50 " + str(int(press_time)))
+    ## is your OS is mac 
+    os.system("../../dependency/platform-tools-macos/adb shell input swipe 50 50 50 50 " + str(int(press_time)))
 
-def jumper():
+def jumper(im):
 
     #ai.init()
     # print(screenShot)
-	distance = 0.0
-	screenShot = Image.open('./autojump.png')
+    distance = 0.0
+    screenShot = Image.open('./autojump.png')
 
     # ******************
     # start your code here
-	piece_x, piece_y, board_x, board_y = find_piece_and_board(im)
+    piece_x, piece_y, board_x, board_y = find_piece_and_board(im)
     distance = math.sqrt((board_x - piece_x) ** 2 + (board_y - piece_y) ** 2)
-	return distance
-
+    return distance
     # ******************
 
 
--- src/jump_python/main.py
+++ src/jump_python/main.py
@@ -2,9 +2,10 @@ from jumper import *
 def main():
     while True:
         im = read_image()
-		im.save("./autojump.png")
+        im.save("./autojump.png")
+        im.show()
         #piece_x, piece_y, board_x, board_y = find_piece_and_board(im)
-		distance = jumper();
+        distance = jumper(im);
         press_time = jump(distance)
         click_screen(press_time)
         time.sleep(2)
